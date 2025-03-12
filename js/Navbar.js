19ce408b616423bd15bcec578ddfb719162b6332
Jonathan <lopezjonathan0407@gmail.com>
2025-03-12 07:19:30 -0400

    Added banner image, navbar. Also configuring Tailwind file

---
+++ js/Navbar.js
@@ -0,0 +1,36 @@
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const bars = document.querySelectorAll("#menu-btn div");
+const menuLinks = document.querySelectorAll("#menu a");
+
+menuBtn.addEventListener("click", () => {
+    // Toggle menu visibility
+    menu.classList.toggle("hidden");
+
+    setTimeout(() => {
+      menu.classList.toggle("opacity-0");
+      menu.classList.toggle("scale-95");
+    }, 10);
+
+    // Animate menu links (fade + slide in)
+    if (!menu.classList.contains("hidden")) {
+      menuLinks.forEach((link, index) => {
+        setTimeout(() => {
+          link.classList.add("opacity-100", "translate-y-0");
+        }, 100 * index);
+      });
+    } else {
+      menuLinks.forEach((link) => {
+        link.classList.remove("opacity-100", "translate-y-0");
+        link.classList.add("opacity-0", "translate-y-5");
+      });
+    }
+
+    // Animate menu button to "X"
+    bars[0].classList.toggle("rotate-45");
+    bars[1].classList.toggle("opacity-0");
+    bars[2].classList.toggle("-rotate-45");
+
+    bars[0].classList.toggle("translate-y-1.5");
+    bars[2].classList.toggle("-translate-y-1.5");
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('../imgs/RingBanner.jpg')",
+        },
+        fontFamily: {
+          fancy: ["Alex Brush", "cursive"], //From Google Fonts
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
